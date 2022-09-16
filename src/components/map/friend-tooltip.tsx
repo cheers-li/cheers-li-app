aa82bb653c81b0af0316e6a70cd9eaa163709696
Kalane <vkaelin@hey.com>
2022-09-16 15:26:42 +0200

    refactor: use active state instead of hover

--- src/components/map/friend-tooltip.tsx
+++ src/components/map/friend-tooltip.tsx
@@ -19,11 +19,11 @@ const FriendTooltip = ({ user }: FriendTooltipProps) => {
         </div>
       </div>
       <div className="mt-2 flex items-center space-x-4 rounded-b-lg border-t border-t-gray-200 bg-gray-100 px-4 py-2 text-xs">
-        <button className="inline-flex items-center justify-between space-x-1 rounded-md border border-sky-600 bg-sky-700 px-4 py-2 text-white hover:bg-sky-800">
+        <button className="inline-flex items-center justify-between space-x-1 rounded-md border border-sky-600 bg-sky-700 px-4 py-2 text-white active:bg-sky-800">
           <ArrowTrendingUpIcon className="h-5 w-5" aria-hidden="true" />
           <span>Join</span>
         </button>
-        <button className="inline-flex items-center justify-between space-x-1 rounded-md border border-sky-600 bg-sky-700 px-4 py-2 text-white hover:bg-sky-800">
+        <button className="inline-flex items-center justify-between space-x-1 rounded-md border border-sky-600 bg-sky-700 px-4 py-2 text-white active:bg-sky-800">
           <ChatBubbleLeftIcon className="h-5 w-5" aria-hidden="true" />
           <span>Chat</span>
         </button>

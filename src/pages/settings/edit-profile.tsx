38a531d18bf1bf1b46c66941d45fe27d00afc36a
Felix Saaro <felix@saaro.ch>
2022-09-29 15:32:44 +0200

    Add avatar upload

--- src/pages/settings/edit-profile.tsx
+++ src/pages/settings/edit-profile.tsx
@@ -1,4 +1,5 @@
 import { CameraIcon } from '@heroicons/react/24/outline';
+import { Camera, CameraResultType } from '@capacitor/camera';
 import { FC, useState } from 'react';
 import { useAsync } from 'react-use';
 import clsx from 'clsx';
@@ -10,6 +11,7 @@ import { PageHeader } from '~/components/page-header';
 import { getStoredUser } from '~/services/auth';
 import { getProfile, updateProfile } from '~/services/profile';
 import { useNavigate } from 'react-router';
+import { generateSimpleKey, uploadAvatar } from '~/services/avatar';
 
 const EditProfile = () => {
   const navigate = useNavigate();
@@ -32,7 +34,41 @@ const EditProfile = () => {
 
   const save = async () => {
     setLoading(true);
-    await updateProfile(profile.value.id, userName, bio, location);
+    await updateProfile({
+      id: profile.value.id,
+      username: userName,
+      bio,
+      city: location,
+    });
+    setLoading(false);
+    navigate(-1);
+  };
+
+  const changePicture = async () => {
+    setLoading(true);
+    const image = await Camera.getPhoto({
+      quality: 50,
+      allowEditing: true,
+      resultType: CameraResultType.DataUrl,
+    });
+
+    if (image.dataUrl) {
+      const key = generateSimpleKey();
+      const { publicURL } = await uploadAvatar(
+        image.dataUrl,
+        `${profile.value.id}-${key}.${image.format}`,
+      );
+      if (publicURL) {
+        await updateProfile({
+          id: profile.value.id,
+          username: userName,
+          bio,
+          city: location,
+          avatarUrl: publicURL,
+        });
+      }
+    }
+
     setLoading(false);
     navigate(-1);
   };
@@ -40,7 +76,7 @@ const EditProfile = () => {
   return (
     <Page hideNavigation>
       <PageHeader
-        LeftComponent={<BackButton />}
+        LeftComponent={<BackButton disabled={loading} />}
         RightComponent={<SaveButton onClick={save} disabled={loading} />}
       >
         Edit Profile
@@ -48,9 +84,9 @@ const EditProfile = () => {
 
       {!profile.loading && (
         <div className="flex flex-col items-center gap-4 px-4">
-          <div className="w-26 relative">
-            <Avatar profile={profile.value} size={26} />
-            <CameraIcon className="absolute bottom-0 right-0 w-8 rounded-full border-2 border-sky-600 bg-sky-600 p-1 text-white" />
+          <div className="w-26 relative" onClick={changePicture}>
+            <Avatar profile={profile.value} size={40} />
+            <CameraIcon className="absolute bottom-0 right-0 w-10 rounded-full border-2 border-sky-600 bg-sky-600 p-1 text-white" />
           </div>
 
           <div className="flex w-full flex-col gap-2">
--- src/services/profile.ts
+++ src/services/profile.ts
@@ -1,4 +1,5 @@
 import dayjs from 'dayjs';
+import { Profile } from './friends';
 import { supabase } from './supabase-client';
 
 export const getProfile = async (userId?: string) => {
@@ -35,20 +36,16 @@ export const createNewProfile = async (userId: string, userName: string) => {
   return { data, error };
 };
 
-export const updateProfile = async (
-  userId: string,
-  userName: string,
-  bio: string,
-  location: string,
-) => {
+export const updateProfile = async (profile: Profile) => {
   const { data, error } = await supabase
     .from('profiles')
     .update({
-      username: userName,
-      bio: bio,
-      city: location,
+      username: profile.username,
+      bio: profile.bio,
+      city: profile.city,
+      avatar_url: profile.avatarUrl,
     })
-    .eq('id', userId);
+    .eq('id', profile.id);
 
   if (error) {
     console.trace();

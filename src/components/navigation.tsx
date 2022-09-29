a660944863048b7da843c3c18321b9beb388123e
Kalane <vkaelin@hey.com>
2022-09-29 09:56:27 +0200

    refactor: add some padding in navigation to make it easier for users

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -11,63 +11,64 @@ import { NavLink } from 'react-router-dom';
 const Navigation = () => {
   return (
     <>
-      <div className="fixed bottom-0 left-0 right-0 border-t border-gray-300 bg-white px-8 py-6">
-        <nav>
-          <ul className="flex items-center justify-between">
-            <li>
-              <NavLink
-                to="/"
-                className={({ isActive }) =>
-                  isActive ? 'text-sky-600' : undefined
-                }
-              >
-                <HomeIcon className="h-8 w-8" aria-hidden="true" />
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/map"
-                className={({ isActive }) =>
-                  isActive ? 'text-sky-600' : undefined
-                }
-              >
-                <MapIcon className="h-8 w-8" aria-hidden="true" />
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/sessions/new"
-                className={({ isActive }) =>
-                  clsx('block rounded-full px-6 py-2 text-white', {
-                    'bg-sky-600': isActive,
-                    'bg-dark-gradient': !isActive,
-                  })
-                }
-              >
-                <PlusCircleIcon className="h-6 w-6" aria-hidden="true" />
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/friends"
-                className={({ isActive }) =>
-                  isActive ? 'text-sky-600' : undefined
-                }
-              >
-                <UsersIcon className="h-8 w-8" aria-hidden="true" />
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/settings"
-                className={({ isActive }) =>
-                  isActive ? 'text-sky-600' : undefined
-                }
-              >
-                <Cog6ToothIcon className="h-8 w-8" aria-hidden="true" />
-              </NavLink>
-            </li>
-          </ul>
+      <div className="fixed bottom-0 left-0 right-0 border-t border-gray-300 bg-white px-5 py-3">
+        <nav className="flex items-center justify-between">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              clsx('p-3', {
+                'text-sky-600': isActive,
+              })
+            }
+          >
+            <HomeIcon className="h-8 w-8" aria-hidden="true" />
+          </NavLink>
+          <NavLink
+            to="/map"
+            className={({ isActive }) =>
+              clsx('p-3', {
+                'text-sky-600': isActive,
+              })
+            }
+          >
+            <MapIcon className="h-8 w-8" aria-hidden="true" />
+          </NavLink>
+          <NavLink
+            to="/sessions/new"
+            className={({ isActive }) =>
+              clsx('group p-3', {
+                activate: isActive,
+              })
+            }
+          >
+            <div
+              className={clsx(
+                'block rounded-full px-6 py-2 text-white [.group.activate_&]:bg-sky-600 [.group:not(.activate)_&]:bg-dark-gradient',
+              )}
+            >
+              <PlusCircleIcon className="h-6 w-6" aria-hidden="true" />
+            </div>
+          </NavLink>
+          <NavLink
+            to="/friends"
+            className={({ isActive }) =>
+              clsx('p-3', {
+                'text-sky-600': isActive,
+              })
+            }
+          >
+            <UsersIcon className="h-8 w-8" aria-hidden="true" />
+          </NavLink>
+          <NavLink
+            to="/settings"
+            className={({ isActive }) =>
+              clsx('p-3', {
+                'text-sky-600': isActive,
+              })
+            }
+          >
+            <Cog6ToothIcon className="h-8 w-8" aria-hidden="true" />
+          </NavLink>
         </nav>
       </div>
     </>

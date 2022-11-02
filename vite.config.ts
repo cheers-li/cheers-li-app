d85db1966640246623920aca3fa5910706493e30
Kalane <vkaelin@hey.com>
2022-11-02 12:23:54 +0100

    chore: bump version

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import Pages from 'vite-plugin-pages';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { ValidateEnv } from '@julr/vite-plugin-validate-env';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    Pages({
-      extendRoute(route, parent) {
-        if (route.path === '/') {
-          // Index is unauthenticated.
-          return route;
-        }
-
-        // Augment the route with meta that indicates that the route requires authentication.
-        return {
-          ...route,
-          meta: { public: true },
-        };
-      },
-    }),
-    tsconfigPaths(),
-    ValidateEnv(),
-  ],
+  plugins: [react(), tsconfigPaths(), ValidateEnv()],
 });
